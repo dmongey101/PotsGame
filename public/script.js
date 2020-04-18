8245e4dc8da5209247c39adf2436cb53c6c7cbbe
Donal Mongey <donal.mongey@tekenable.com>
2020-04-18 18:33:41 +0100

    added prod url to html

--- public/script.js
+++ public/script.js
@@ -226,7 +226,7 @@ socket.on('counter', count => {
 })
 
 socket.on('back-home', () => {
-  var url = 'http://localhost:3000/rooms'
+  var url = 'https://com-pots.herokuapp.com/rooms'
   window.location = url 
 })
 
@@ -235,7 +235,7 @@ socket.on('user-disconnected', player => {
 })
 
 socket.on('start-game', room => {
-  var url = `http://localhost:3000/${room}/start/1`
+  var url = `https://com-pots.herokuapp.com/${room}/start/1`
   window.location = url
 })
 
